2f38c51d515f0bf893c69cd4a90cac06086294e0
Filip Czaplicki <filipczaplicki@gmail.com>
2017-02-06 13:15:03 +0100

    Scripts: GameManager init.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public GameObject[] tiles;
+
+	// Use this for initialization
+	void Start () {
+		foreach (GameObject tile in tiles) {
+			Instantiate (tile);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
