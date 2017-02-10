aa21189b99fae87abab67cbe1acf8d00b70bbcdf
Filip Czaplicki <filipczaplicki@gmail.com>
2017-02-11 00:56:13 +0100

    Editor: Archiver, BuildSettings reformat, refactor.

--- Assets/Editor/Archiver.cs
+++ Assets/Editor/Archiver.cs
@@ -1,80 +1,79 @@
 ﻿using System.IO;
-
-using ICSharpCode.SharpZipLib.Tar;
-using ICSharpCode.SharpZipLib.GZip;
-using ICSharpCode.SharpZipLib.Zip;
 using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.GZip;
+using ICSharpCode.SharpZipLib.Tar;
+using ICSharpCode.SharpZipLib.Zip;
 
-public static class Archiver {
-	const string TAR_EXTENSION = ".tar.gz";
-	const string ZIP_EXTENSION = ".zip";
+namespace Editor {
+    public static class Archiver {
+        private const string TarExtension = ".tar.gz";
+        private const string ZipExtension = ".zip";
 
-	public static void CreateTar (string outputTarFilename, string sourceDirectory) {
-		Directory.SetCurrentDirectory (BuildSettings.DIRECTORY);
-		outputTarFilename += TAR_EXTENSION;
-		using (FileStream fs = new FileStream (outputTarFilename, FileMode.Create, FileAccess.Write, FileShare.None))
-		using (Stream gzipStream = new GZipOutputStream (fs))
-		using (TarArchive tarArchive = TarArchive.CreateOutputTarArchive (gzipStream)) {
-			AddDirectoryFilesToTar (tarArchive, sourceDirectory, true);
-		}
-		Directory.SetCurrentDirectory ("..");
-	}
+        public static void CreateTar(string outputTarFilename, string sourceDirectory) {
+            Directory.SetCurrentDirectory(BuildSettings.Directory);
+            outputTarFilename += TarExtension;
+            using (var fs = new FileStream(outputTarFilename, FileMode.Create, FileAccess.Write, FileShare.None))
+            using (var gzipStream = new GZipOutputStream(fs))
+            using (var tarArchive = TarArchive.CreateOutputTarArchive(gzipStream)) {
+                AddDirectoryFilesToTar(tarArchive, sourceDirectory);
+            }
+            Directory.SetCurrentDirectory("..");
+        }
 
-	private static void AddDirectoryFilesToTar (TarArchive tarArchive, string sourceDirectory, bool recurse) {
-		if (recurse) {
-			string[] directories = Directory.GetDirectories (sourceDirectory);
-			foreach (string directory in directories)
-				AddDirectoryFilesToTar (tarArchive, directory, recurse);
-		}
-		string[] filenames = Directory.GetFiles (sourceDirectory);
-		foreach (string filename in filenames) {
-			TarEntry tarEntry = TarEntry.CreateEntryFromFile (filename);
-			tarArchive.WriteEntry (tarEntry, true);
-		}
-	}
+        private static void AddDirectoryFilesToTar(TarArchive tarArchive, string sourceDirectory) {
+            var directories = Directory.GetDirectories(sourceDirectory);
+            foreach (var directory in directories)
+                AddDirectoryFilesToTar(tarArchive, directory);
 
-	public static void CreateZip (string outputZipFilename, string sourceDirectory) {
-		Directory.SetCurrentDirectory (BuildSettings.DIRECTORY);
-		outputZipFilename += ZIP_EXTENSION;
+            var filenames = Directory.GetFiles(sourceDirectory);
+            foreach (var filename in filenames) {
+                var tarEntry = TarEntry.CreateEntryFromFile(filename);
+                tarArchive.WriteEntry(tarEntry, true);
+            }
+        }
 
-		FileStream fsOut = File.Create (outputZipFilename);
-		ZipOutputStream zipStream = new ZipOutputStream (fsOut);
+        public static void CreateZip(string outputZipFilename, string sourceDirectory) {
+            Directory.SetCurrentDirectory(BuildSettings.Directory);
+            outputZipFilename += ZipExtension;
 
-		zipStream.SetLevel (3); //0-9, 9 being the highest level of compression
+            var fsOut = File.Create(outputZipFilename);
+            var zipStream = new ZipOutputStream(fsOut);
 
-		CompressFolder (sourceDirectory, zipStream, 0);
+            zipStream.SetLevel(3); //0-9, 9 being the highest level of compression
 
-		zipStream.IsStreamOwner = true;
-		zipStream.Close ();
+            CompressFolder(sourceDirectory, zipStream, 0);
 
-		Directory.SetCurrentDirectory ("..");
-	}
+            zipStream.IsStreamOwner = true;
+            zipStream.Close();
 
-	private static void CompressFolder (string path, ZipOutputStream zipStream, int folderOffset) {
+            Directory.SetCurrentDirectory("..");
+        }
 
-		string[] files = Directory.GetFiles (path);
+        private static void CompressFolder(string path, ZipOutputStream zipStream, int folderOffset) {
+            var files = Directory.GetFiles(path);
 
-		foreach (string filename in files) {
+            foreach (var filename in files) {
+                var fi = new FileInfo(filename);
 
-			FileInfo fi = new FileInfo (filename);
+                var entryName = filename.Substring(folderOffset);
+                entryName = ZipEntry.CleanName(entryName);
+                var newEntry = new ZipEntry(entryName) {
+                    DateTime = fi.LastWriteTime,
+                    Size = fi.Length
+                };
 
-			string entryName = filename.Substring (folderOffset);
-			entryName = ZipEntry.CleanName (entryName);
-			ZipEntry newEntry = new ZipEntry (entryName);
-			newEntry.DateTime = fi.LastWriteTime;
-			newEntry.Size = fi.Length;
+                zipStream.PutNextEntry(newEntry);
 
-			zipStream.PutNextEntry (newEntry);
-
-			byte[] buffer = new byte[4096];
-			using (FileStream streamReader = File.OpenRead (filename)) {
-				StreamUtils.Copy (streamReader, zipStream, buffer);
-			}
-			zipStream.CloseEntry ();
-		}
-		string[] folders = Directory.GetDirectories (path);
-		foreach (string folder in folders) {
-			CompressFolder (folder, zipStream, folderOffset);
-		}
-	}
+                var buffer = new byte[4096];
+                using (var streamReader = File.OpenRead(filename)) {
+                    StreamUtils.Copy(streamReader, zipStream, buffer);
+                }
+                zipStream.CloseEntry();
+            }
+            var folders = Directory.GetDirectories(path);
+            foreach (var folder in folders) {
+                CompressFolder(folder, zipStream, folderOffset);
+            }
+        }
+    }
 }
--- Assets/Editor/BuildSettings.cs
+++ Assets/Editor/BuildSettings.cs
@@ -1,219 +1,220 @@
-﻿using UnityEngine;
+﻿using System.IO;
 using UnityEditor;
-using System.Collections;
-using System.IO;
+using UnityEngine;
 
-public static class BuildTargetExtension {
-	public static string Name (this BuildTarget target) {
-		switch (target) {
-		case BuildTarget.StandaloneLinux:
-			return "Linux32";
-		case BuildTarget.StandaloneLinux64:
-			return "Linux64";
-		case BuildTarget.StandaloneWindows:
-			return "Win32";
-		case BuildTarget.StandaloneWindows64:
-			return "Win64";
-		case BuildTarget.Android:
-			return "Android";
-		case BuildTarget.WebGL:
-			return "WebGL";
-		default:
-			return "Unknown";
-		}
-	}
+namespace Editor {
+    public static class BuildTargetExtension {
+        public static string Name(this BuildTarget target) {
+            switch (target) {
+                case BuildTarget.StandaloneLinux:
+                    return "Linux32";
+                case BuildTarget.StandaloneLinux64:
+                    return "Linux64";
+                case BuildTarget.StandaloneWindows:
+                    return "Win32";
+                case BuildTarget.StandaloneWindows64:
+                    return "Win64";
+                case BuildTarget.Android:
+                    return "Android";
+                case BuildTarget.WebGL:
+                    return "WebGL";
+                default:
+                    return "Unknown";
+            }
+        }
 
-	public static string Extension (this BuildTarget target) {
-		switch (target) {
-		case BuildTarget.StandaloneWindows:
-		case BuildTarget.StandaloneWindows64:
-			return ".exe";
-		case BuildTarget.Android:
-			return ".apk";
-		default:
-			return "";
-		}
-	}
-}
+        public static string Extension(this BuildTarget target) {
+            switch (target) {
+                case BuildTarget.StandaloneWindows:
+                case BuildTarget.StandaloneWindows64:
+                    return ".exe";
+                case BuildTarget.Android:
+                    return ".apk";
+                default:
+                    return "";
+            }
+        }
+    }
 
-class BuildSettings : MonoBehaviour {
-	public const string VERSION = "v0.2.0";
-	public const string PROJECT_NAME = "2048";
-	public const string DIRECTORY = "Build";
-	private const string GITHUB_PAGES_DIRECTORY = "docs";
+    public class BuildSettings : MonoBehaviour {
+        private const string Version = "v0.2.0";
+        private const string ProjectName = "2048";
+        public const string Directory = "Build";
+        private const string GithubPagesDirectory = "docs";
 
-	private static string[] levels = new string[] {
-		"Assets/Scenes/Game.unity"
-	};
+        private static readonly string[] Levels = {
+            "Assets/Scenes/Game.unity"
+        };
 
-	private static string DirectoryName (string platform) {
-		return PROJECT_NAME + "-" + platform + "-" + VERSION;
-	}
+        private static string DirectoryName(string platform) {
+            return ProjectName + "-" + platform + "-" + Version;
+        }
 
-	private static string DirectoryName (BuildTarget target) {
-		return DirectoryName (target.Name ());
-	}
+        private static string DirectoryName(BuildTarget target) {
+            return DirectoryName(target.Name());
+        }
 
-	private static void MakeAction (ActionAfterBuild action, BuildTarget target, string extension = "") {
-		string dir = DirectoryName (target);
-		switch (action) {
-		case ActionAfterBuild.Tar:
-			Archiver.CreateTar (dir, dir);
-			return;
-		case ActionAfterBuild.Zip:
-			Archiver.CreateZip (dir, dir);
-			return;
-		case ActionAfterBuild.MoveApk:
-			MoveApk (dir);
-			return;
-		case ActionAfterBuild.MoveDirDeployAndZip:
-			MoveDir (dir);
-			DeployGithubPages (dir);
-			Archiver.CreateZip (dir, dir);
-			return;
-		default:
-			return;
-		}
-	}
+        private static void MakeAction(ActionAfterBuild action, BuildTarget target) {
+            var dir = DirectoryName(target);
+            switch (action) {
+                case ActionAfterBuild.Tar:
+                    Archiver.CreateTar(dir, dir);
+                    return;
+                case ActionAfterBuild.Zip:
+                    Archiver.CreateZip(dir, dir);
+                    return;
+                case ActionAfterBuild.MoveApk:
+                    MoveApk(dir);
+                    return;
+                case ActionAfterBuild.MoveDirDeployAndZip:
+                    MoveDir(dir);
+                    DeployGithubPages(dir);
+                    Archiver.CreateZip(dir, dir);
+                    return;
+                default:
+                    return;
+            }
+        }
 
-	private static void Build (BuildTarget target, ActionAfterBuild action) {
-		UnityEditor.PlayerSettings.runInBackground = false;
-		char separator = Path.DirectorySeparatorChar;
-		string platform = target.Name ();
-		string directory = DirectoryName (platform);
-		string path = DIRECTORY + separator + directory + separator + PROJECT_NAME + target.Extension ();
-		string message = BuildPipeline.BuildPlayer (levels, path, target, BuildOptions.None);
-		if (string.IsNullOrEmpty (message)) {
-			UnityEngine.Debug.Log (platform + " build complete");
-			MakeAction (action, target);
-		} else {
-			UnityEngine.Debug.LogError ("Error building " + platform + ":\n" + message);
-		}
-	}
+        private static void Build(BuildTarget target, ActionAfterBuild action) {
+            PlayerSettings.runInBackground = false;
+            var separator = Path.DirectorySeparatorChar;
+            var platform = target.Name();
+            var directory = DirectoryName(platform);
+            var path = Directory + separator + directory + separator + ProjectName + target.Extension();
+            var message = BuildPipeline.BuildPlayer(Levels, path, target, BuildOptions.None);
+            if (string.IsNullOrEmpty(message)) {
+                Debug.Log(platform + " build complete");
+                MakeAction(action, target);
+            }
+            else {
+                Debug.LogError("Error building " + platform + ":\n" + message);
+            }
+        }
 
-	private enum ActionAfterBuild {
-		Zip,
-		Tar,
-		MoveApk,
-		MoveDirDeployAndZip,
-		None
-	}
+        private enum ActionAfterBuild {
+            Zip,
+            Tar,
+            MoveApk,
+            MoveDirDeployAndZip
+        }
 
-	private static void MoveApk (string dir) {
-		Directory.SetCurrentDirectory (BuildSettings.DIRECTORY);
-		string target = dir + ".apk";
-		if (File.Exists (target)) {
-			File.Delete (target);
-		}
-		File.Move (dir + Path.DirectorySeparatorChar + PROJECT_NAME + ".apk", target);
-		Directory.Delete (dir, true);
-		Directory.SetCurrentDirectory ("..");
-	}
+        private static void MoveApk(string dir) {
+            System.IO.Directory.SetCurrentDirectory(Directory);
+            var target = dir + ".apk";
+            if (File.Exists(target)) {
+                File.Delete(target);
+            }
+            File.Move(dir + Path.DirectorySeparatorChar + ProjectName + ".apk", target);
+            System.IO.Directory.Delete(dir, true);
+            System.IO.Directory.SetCurrentDirectory("..");
+        }
 
-	private static void MoveDir (string dir) {
-		string startDirectory = Directory.GetCurrentDirectory ();
-		Directory.SetCurrentDirectory (BuildSettings.DIRECTORY + Path.DirectorySeparatorChar +
-		dir + Path.DirectorySeparatorChar + PROJECT_NAME);
-		string[] files = Directory.GetFiles (".");
-		foreach (string filename in files) {
-			string target = ".." + Path.DirectorySeparatorChar + filename;
-			if (File.Exists (target)) {
-				File.Delete (target);
-			}
-			File.Move (filename, target);
-		}
-		string[] directories = Directory.GetDirectories (".");
-		foreach (string directory in directories) {
-			string target = ".." + Path.DirectorySeparatorChar + directory;
-			if (Directory.Exists (target)) {
-				Directory.Delete (target, true);
-			}
-			Directory.Move (directory, target);
-		}
-		Directory.SetCurrentDirectory ("..");
-		Directory.Delete (PROJECT_NAME, true);
-		Directory.SetCurrentDirectory (startDirectory);
-	}
+        private static void MoveDir(string dir) {
+            var startDirectory = System.IO.Directory.GetCurrentDirectory();
+            System.IO.Directory.SetCurrentDirectory(Directory + Path.DirectorySeparatorChar +
+                                                    dir + Path.DirectorySeparatorChar + ProjectName);
+            var files = System.IO.Directory.GetFiles(".");
+            foreach (var filename in files) {
+                var target = ".." + Path.DirectorySeparatorChar + filename;
+                if (File.Exists(target)) {
+                    File.Delete(target);
+                }
+                File.Move(filename, target);
+            }
+            var directories = System.IO.Directory.GetDirectories(".");
+            foreach (var directory in directories) {
+                var target = ".." + Path.DirectorySeparatorChar + directory;
+                if (System.IO.Directory.Exists(target)) {
+                    System.IO.Directory.Delete(target, true);
+                }
+                System.IO.Directory.Move(directory, target);
+            }
+            System.IO.Directory.SetCurrentDirectory("..");
+            System.IO.Directory.Delete(ProjectName, true);
+            System.IO.Directory.SetCurrentDirectory(startDirectory);
+        }
 
-	private static void DeployGithubPages (string dir) {
-		if (Directory.Exists (GITHUB_PAGES_DIRECTORY)) {
-			Directory.Delete (GITHUB_PAGES_DIRECTORY, true);
-			Directory.CreateDirectory (GITHUB_PAGES_DIRECTORY);
-		}
-		string startDirectory = Directory.GetCurrentDirectory ();
-		Directory.SetCurrentDirectory (BuildSettings.DIRECTORY + Path.DirectorySeparatorChar + dir);
-		string[] files = Directory.GetFiles (".");
-		string up = ".." + Path.DirectorySeparatorChar;
-		foreach (string filename in files) {
-			File.Copy (filename, up + up + GITHUB_PAGES_DIRECTORY + Path.DirectorySeparatorChar + filename);
-		}
-		string[] directories = Directory.GetDirectories (".");
-		foreach (string directory in directories) {
-			DirectoryCopy (directory, up + up + GITHUB_PAGES_DIRECTORY + Path.DirectorySeparatorChar + directory);
-		}
-		Directory.SetCurrentDirectory (startDirectory);
-	}
+        private static void DeployGithubPages(string dir) {
+            if (System.IO.Directory.Exists(GithubPagesDirectory)) {
+                System.IO.Directory.Delete(GithubPagesDirectory, true);
+                System.IO.Directory.CreateDirectory(GithubPagesDirectory);
+            }
+            var startDirectory = System.IO.Directory.GetCurrentDirectory();
+            System.IO.Directory.SetCurrentDirectory(Directory + Path.DirectorySeparatorChar + dir);
+            var files = System.IO.Directory.GetFiles(".");
+            var up = ".." + Path.DirectorySeparatorChar;
+            foreach (var filename in files) {
+                File.Copy(filename, up + up + GithubPagesDirectory + Path.DirectorySeparatorChar + filename);
+            }
+            var directories = System.IO.Directory.GetDirectories(".");
+            foreach (var directory in directories) {
+                DirectoryCopy(directory, up + up + GithubPagesDirectory + Path.DirectorySeparatorChar + directory);
+            }
+            System.IO.Directory.SetCurrentDirectory(startDirectory);
+        }
 
-	private static void DirectoryCopy (string sourceDirName, string destDirName) {
-		DirectoryInfo dir = new DirectoryInfo (sourceDirName);
+        private static void DirectoryCopy(string sourceDirName, string destDirName) {
+            var dir = new DirectoryInfo(sourceDirName);
 
-		DirectoryInfo[] dirs = dir.GetDirectories ();
-		// If the destination directory doesn't exist, create it.
-		if (!Directory.Exists (destDirName)) {
-			Directory.CreateDirectory (destDirName);
-		}
+            var dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!System.IO.Directory.Exists(destDirName)) {
+                System.IO.Directory.CreateDirectory(destDirName);
+            }
 
-		// Get the files in the directory and copy them to the new location.
-		FileInfo[] files = dir.GetFiles ();
-		foreach (FileInfo file in files) {
-			string temppath = Path.Combine (destDirName, file.Name);
-			file.CopyTo (temppath, false);
-		}
+            // Get the files in the directory and copy them to the new location.
+            var files = dir.GetFiles();
+            foreach (var file in files) {
+                var temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
 
 
-		foreach (DirectoryInfo subdir in dirs) {
-			string temppath = Path.Combine (destDirName, subdir.Name);
-			DirectoryCopy (subdir.FullName, temppath);
-		}
-	}
+            foreach (var subdir in dirs) {
+                var temppath = Path.Combine(destDirName, subdir.Name);
+                DirectoryCopy(subdir.FullName, temppath);
+            }
+        }
 
-	[MenuItem ("Build/Linux32")]
-	public static void BuildLinux32 () {
-		Build (BuildTarget.StandaloneLinux, ActionAfterBuild.Tar);
-	}
+        [MenuItem("Build/Linux32")]
+        public static void BuildLinux32() {
+            Build(BuildTarget.StandaloneLinux, ActionAfterBuild.Tar);
+        }
 
-	[MenuItem ("Build/Linux64")]
-	public static void BuildLinux64 () {
-		Build (BuildTarget.StandaloneLinux64, ActionAfterBuild.Tar);
-	}
+        [MenuItem("Build/Linux64")]
+        public static void BuildLinux64() {
+            Build(BuildTarget.StandaloneLinux64, ActionAfterBuild.Tar);
+        }
 
-	[MenuItem ("Build/Win32")]
-	public static void BuildWin32 () {
-		Build (BuildTarget.StandaloneWindows, ActionAfterBuild.Zip);
-	}
+        [MenuItem("Build/Win32")]
+        public static void BuildWin32() {
+            Build(BuildTarget.StandaloneWindows, ActionAfterBuild.Zip);
+        }
 
-	[MenuItem ("Build/Win64")]
-	public static void BuildWin64 () {
-		Build (BuildTarget.StandaloneWindows64, ActionAfterBuild.Zip);
-	}
+        [MenuItem("Build/Win64")]
+        public static void BuildWin64() {
+            Build(BuildTarget.StandaloneWindows64, ActionAfterBuild.Zip);
+        }
 
-	[MenuItem ("Build/Android")]
-	public static void BuildAndroid () {
-		Build (BuildTarget.Android, ActionAfterBuild.MoveApk);
-	}
+        [MenuItem("Build/Android")]
+        public static void BuildAndroid() {
+            Build(BuildTarget.Android, ActionAfterBuild.MoveApk);
+        }
 
-	[MenuItem ("Build/WebGL")]
-	public static void BuildWebGL () {
-		Build (BuildTarget.WebGL, ActionAfterBuild.MoveDirDeployAndZip);
-	}
+        [MenuItem("Build/WebGL")]
+        public static void BuildWebGl() {
+            Build(BuildTarget.WebGL, ActionAfterBuild.MoveDirDeployAndZip);
+        }
 
-	[MenuItem ("Build/All")]
-	public static void BuildAll () {
-		BuildLinux32 ();
-		BuildLinux64 ();
-		BuildWin32 ();
-		BuildWin64 ();
-		BuildAndroid ();
-		BuildWebGL ();
-	}
+        [MenuItem("Build/All")]
+        public static void BuildAll() {
+            BuildLinux32();
+            BuildLinux64();
+            BuildWin32();
+            BuildWin64();
+            BuildAndroid();
+            BuildWebGl();
+        }
+    }
 }
